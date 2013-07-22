2a2b4489c45ba6b18ac7ce0069e432fad4fed15e
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2013-07-23 00:51:50 +0200

    fixed

--- _installation.sh
+++ _installation.sh
@@ -7,13 +7,13 @@ chmod 7777 /etc/init.d/local.autostart
 
 chmod 7777 listener.py
 chmod 7777 listener_start.py
-chmod 7777 temp.py
+chmod 7777 temperature.py
 
 chmod 7777 script_watcher.sh
 
 rm listener.pyc
 rm listener_start.pyc
-rm temp.pyc
+rm temperature.pyc
 mkdir /root/pictures
 
 gcc wdt_starter.c -o wdt_starter -I /usr/include/python2.6 -l python2.6
