2f4f8a87d917682e2a61b8549ae2b4bb2ac76227
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2013-07-23 13:56:52 +0200

    fixes

--- temperature.py
+++ temperature.py
@@ -1,67 +1,35 @@
 #! /usr/bin/python
 import os
-import time
 
 class Temperature(object):
 	
 	def __init__(self):
 		self.device = "/dev/lpc313x_adc"
-<<<<<<< HEAD
-    self.select_gpa()
+		self.select_gpa()
 	
 	def select_gpa(self):
-		"""
-            Waehlt einen AD-Kanal aus.
-            """
-    adc_file = os.open(self.device, os.RDWR)
-    os.write(adc_file, "0x0001")
-    os.close(adc_file)
-	
-	def get_adc(self):
-		"""
-            Liest den Wert des AD-Wandlers aus
-            """
-    adc_file = os.open(self.device, os.O_RDONLY)
-    result = os.read(adc_file, 256)
-    os.close(adc_file)
-    return result[:-1]
-    
-	def calculateTemperature(self):
-		"""
-            Berechnet die Temperatur aus einem Hexadezimalen String
-            """
-    try:
-        temp = int(self.get_adc(), 16)
-        return temp
-    except Exception as e:
-        raise TemperatureException("Can't calculate the temperature")
-=======
-        self.select_gpa()
-	
-	def select_gpa(self):
-		"""
-		Waehlt einen AD-Kanal aus.
-		"""
-        adc_file = os.open(self.device, os.RDWR)
-        os.write(adc_file, "0x0001")
-        os.close(adc_file)
-	
+	"""
+	Waehlt einen AD-Kanal aus.
+	"""
+	adc_file = os.open(self.device, os.RDWR)
+	os.write(adc_file, "0x0001")
+	os.close(adc_file)
+
 	def get_adc(self):
 		"""
 		Liest den Wert des AD-Wandlers aus
 		"""
-        adc_file = os.open(self.device, os.O_RDONLY)
-        result = os.read(adc_file, 256)
-        os.close(adc_file)
-        return result[:-1]
+		adc_file = os.open(self.device, os.O_RDONLY)
+		result = os.read(adc_file, 256)
+		os.close(adc_file)
+		return result[:-1]
 
 	def calculateTemperature(self):
 		"""
 		Berechnet die Temperatur aus einem Hexadezimalen String
 		"""
-        try:
+		try:
 			temp = int(self.get_adc(), 16)
 			return temp
-        except Exception as e:
-            raise TemperatureException("Can't calculate the temperature")
->>>>>>> 3b8cb4b7f0324e1f362143aee792276fb9b6c864
+		except Exception as e:
+			raise TemperatureException("Can't calculate the temperature")
