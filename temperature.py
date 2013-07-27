ea04a876c850238626ad53420fb34eb13ea64654
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2013-07-28 01:26:40 +0200

    Calculate the temperature

--- temperature.py
+++ temperature.py
@@ -1,11 +1,18 @@
 #! /usr/bin/python
 import os
+import math
 
 class Temperature(object):
+	
+	A = 3.9083E-3
+	B = -5.775E-7
+	C = -4.183E-12
+	R0 = 1000
 	
 	def __init__(self):
 		self.device = "/dev/lpc313x_adc"
 		self.select_gpa()
+		self.temperature = 0
 	
 	def select_gpa(self):
 		"""
@@ -23,8 +30,21 @@ class Temperature(object):
 		result = os.read(adc_file, 256)
 		os.close(adc_file)
 		return result[:-1]
+	
+	def calcR():
+		"""
+		Berechnet den temperaturabhängigen Widerstand
+		"""
+		mV = int(self.get_adc(), 16)
+		i = int(self.get_adc(), 16)/1000
+		resistence = (3.3 - mV)/i
+		return resistence
+	
+	def calculateTemperature():
+		self.temperature = ((-A*R0)+(math.sqrt(((A*R0)*(A*R0))-(4*B*R0*(R0-self.calcR())))))/(2*B*R0)
+		return self.temperature
 
-	def calculateTemperature(self, amountOfValues):
+	def getAverageOfTemperature(self, amountOfValues):
 		"""
 		Berechnet die Temperatur aus einem Hexadezimalen String
 		"""
@@ -32,7 +52,7 @@ class Temperature(object):
 			count = 0
 			temp = 0
 			while count != amountOfValues:
-				temp += int(self.get_adc(), 16)
+				temp += self.calculateTemperature()
 				count++
 			
 			return (temp / amountOfValues)
