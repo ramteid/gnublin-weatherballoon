5bfcaa0f626e8f63a241178f944a92211e060512
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2013-07-27 19:08:12 +0200

    Sends all 10 seconds an impulse to the micro controller

---
+++ impulse.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Sends all 10 seconds an impulse to the micro controller
+
+GPIO=3
+
+cleanup() { # Release the GPIO port
+	echo $GPIO > /sys/class/gpio/unexport
+	exit
+}
+
+# Open the GPIO port ('high' direction is output)
+echo $GPIO > /sys/class/gpio/export
+echo "high" > /sys/class/gpio/gpio$GPIO/direction
+
+trap cleanup SIGINT # call cleanup on Ctrl-C
+
+while [ "1" = "1" ];
+do
+	echo 1 > /sys/class/gpio/gpio$GPIO/value
+	sleep 1
+	echo 0 > /sys/class/gpio/gpio$GPIO/value
+	sleep 10
+done
+
+cleanup # call the cleanup routine
