2f9bd441ca33d3a7400d93261cd14a16ec73db98
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2013-07-28 22:10:12 +0200

    Use gnublin-adcint -b 1 to get the voltage of ADC

--- temperature.py
+++ temperature.py
@@ -1,49 +1,32 @@
 #! /usr/bin/python
 import os
 from math import sqrt
+from subprocess import Popen, PIPE
 
 class Temperature(object):
-
-	# Platinum temperature coefficients
-	# DIN EN 60751 / IEC 60751
-	A = 3.9083E-3
-	B = -5.775E-7
-	C = -4.183E-12
-	R0 = 1000
-	
-	# Initialize the device and select the AD channel
-	def __init__(self):
-		self.device = "/dev/lpc313x_adc"
-		self.select_gpa()
-	
-	# Selects the AD channel
-	def select_gpa(self):
-		adc_file = os.open(self.device, os.RDWR)
-		os.write(adc_file, "0x0001")
-		os.close(adc_file)
-
-	# Selects the value of the AD converter
-	def get_adc(self):
-		adc_file = os.open(self.device, os.O_RDONLY)
-		result = os.read(adc_file, 256)
-		os.close(adc_file)
-		return result[:-1]
 	
 	# Calculates the resistence dependent on temperature
-	# I = U/R
-	# R = U/I
+	# I = V/R
+	# R = V/I
 	def calcR(self):
-		mV = int(self.get_adc(), 16)
-		return (3300 - mV)/(mV/1000.0)
+		# Execute command and write the result to stdout
+		(stdout, stderr) = Popen(["gnublin-adcint","-b","1"], stdout=PIPE).communicate()
+		V = float(stdout) / 1000
+		return (3.3 - V) / (V / 1000)
 	
 	# Calculates the temperature with the help of
-	# the platinum temperature coefficients
+	# the platinum temperature coefficients.
+	# Platinum temperature coefficients
+	# DIN EN 60751 / IEC 60751
 	def calculateTemperature(self):
-		a = self.B * self.R0
-		b = self.A * self.R0
-		c = self.R0 - self.calcR()
-		return (-b+(sqrt((b**2)-(4.0*a*c)))/(2.0*a)
-
+		A = 3.9083E-3
+		B = -5.775E-7
+		C = -4.183E-12
+		R0 = 1000          # resistence at 0 Celsius
+		R = self.calcR()
+		T = ((-A*R0)+(sqrt(((A*R0)*(A*R0))-(4*B*R0*(R0-R)))))/(2*B*R0)
+		return T
+	
 	# Returns the average temperature of a given amount of temperatures
 	def getAverageOfTemperature(self, amountOfValues):
 		try:
