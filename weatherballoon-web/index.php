8ec47f4f40510e5a437db92bc3f8d05cd1e68f4d
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2013-07-29 19:48:51 +0200

    layout

---
+++ weatherballoon-web/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Gnublin-Weatherballoon</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+		<!-- CSS -->
+		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen" type="text/css" />
+		<link href="css/layout.css" rel="stylesheet" media="screen" type="text/css" />
+	
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	    <!--[if lt IE 9]>
+	    	<script src="js/html5shiv.js"></script>
+	    <![endif]-->
+		
+		<!-- Javascript -->
+		<script src="http://code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</head>
+	<body lang="de">
+		<div class="container-narrow">
+			<div class="navbar navbar-fixed-top">
+				<div class="navbar-inner">
+					<div class="container">
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+						
+						<a class="brand">gnublin weatherballoon</a>
+						
+						<div class="nav-collapse">
+							<ul class="nav pull-right">
+								<li class="active"><a href="#">Home</a></li>
+								<li class="dropdown">
+									<a class="dropdown-toggle" href="#" data-toggle="dropdown">Login <strong class="caret"></strong></a>
+									<div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+										
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr>
+			
+		</div>
+	</body>
+</html>
