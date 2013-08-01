7af814a07167f56bb5ed8c5b61f4b8b173d48898
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2013-08-01 11:58:12 +0200

    impulse

--- _installation.sh
+++ _installation.sh
@@ -31,9 +31,9 @@ rm gpsParser.pyc
 mkdir /root/pictures
 mkdir /root/logs
 
-# compile the c/c++ files
+# compile the c files
 gcc wdt_starter.c -o wdt_starter -I /usr/include/python2.6 -l python2.6
-g++ impulse.cpp -o impulse
+gcc impulse.c -o impulse
 
 # create dynamic temperature library
 gcc -fPIC -Wall -g -c temperature.c
---
+++ impulse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+inc main()
+{
+	if (system(NULL)) {
+		while (1) {
+			system("gnublin-gpio -p 11 -o 1");
+			sleep(2);
+			system("gnublin-gpio -p 11 -o 0");
+			sleep(10);
+		}
+	} else {
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
